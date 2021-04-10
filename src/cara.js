da49ef52ccb8ca47173fcb457cb517a9a49fc67d
JadzBotz <82241365+JadzBotz@users.noreply.github.com>
2021-04-09 23:15:08 -0300

    Add files via upload

---
+++ src/cara.js
@@ -0,0 +1,47 @@
+const cara = (pushname, prefix, botName, ownerName) => { 
+	return `♨️ -----[ *GUIA DE USO ${botName}* ]----- ♨️
+Opa, ${pushname} 👋
+Veja como usar o *${botName}*
+         ────────────────
+Aqui você vai ver como usar alguns comandos :V
+         ────────────────
+➸ *${prefix}sticker*
+Envie fotos / vídeos / gifs com legendas sticker.
+➸ *${prefix}ttp*
+Transforma texto em sticker, Exemplo : ${prefix}ttp Jadz Rebaixadokkk
+➸ *${prefix}tts*
+Voz do Google, Exemplo : ${prefix}tts pt Iae Jadz
+➸ *${prefix}toimg*
+Transforma um sticker em foto (Só funciona com sticker que não se mexe).
+➸ *${prefix}nulis*
+Você pode escrever um livro ¯\_(ツ)_/¯
+➸ *${prefix}stalkig*
+Stalking instagram, Exemplo : ${prefix}stalkig iamramlan_
+➸ *${prefix}quotes*
+Quotes aleatórios.
+➸ *${prefix}bikinquote*
+Pode fazrr seu quote, Exemplo : ${prefix}bikinquote Jadz Rebaixadokkk & Jadz
+➸ *${prefix}play*
+Baixar músicas do YouTube pode usar texto ou links.
+➸ *${prefix}yutubdl*
+Baixe vídeos do youtube, usando o link.
+➸ *${prefix}tiktod*
+Baixe o vídeo do tiktok, usando link.
+➸ *${prefix}hartatahta*
+Faça logos, Exemplo : ${prefix}hartatahta Jadz
+➸ *${prefix}pornhub*
+Faça logos estilo pornhub, Exemplo : ${prefix}pornhub Jadz & Hub
+Nota : para criar o logotipo / Você precisa usar o "& Hub" no final.
+➸ *${prefix}fitnah*
+Único grupo para calúnia de gente, Exemplo : ${prefix}fitnah @tagtarget & Hai & Hai juga
+(Não sei pra que serve ¯\_(ツ)_/¯ )
+➸ *${prefix}mutual*
+Número da Gacha que está no banco de dados / de quem usa o bot
+         ────────────────
+Nota : Tem muitos comandos, más aí tá os mais "basicos", tenha um bom uso do bot :)
+         ────────────────
+♨️ -----[ *    BY JADZ    * ]----- ♨️
+`
+}
+
+exports.cara = cara
